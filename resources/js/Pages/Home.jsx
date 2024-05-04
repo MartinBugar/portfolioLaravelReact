dcc25950c4bf6e338a9c3efc5ee6eb6e15027313
Martin <martin1.bugar@gmail.com>
2024-05-04 17:28:42 +0200

    initial

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+K
+
+    render() {
+        return (
+            <>
+                AAAdsfsd
+            </>
+        );
+    }
+}
